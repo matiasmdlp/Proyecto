06a09be8f9cb4ddda3e7c377f23b290ed0122a37
matiasmdlp <matiasmdlp95@gmail.com>
2022-11-20 01:06:20 -0300

    Timers
    timer para la animacion principal y otro para el lanzamiento

--- Proyecto/src/proyecto/Ventana.java
+++ Proyecto/src/proyecto/Ventana.java
@@ -1,14 +1,23 @@
 package proyecto;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.Timer;
 
-public class Ventana extends JFrame{
+public class Ventana extends JFrame implements ActionListener{
     private final PanelFondo fondo;
+    private Timer timer, timer2;
+    
     public Ventana(){
         super();
         this.setLayout(new BorderLayout());
         this.setTitle("Lanzamiento de misil");
+        timer = new Timer(100,null);
+        timer.addActionListener(this);
+        timer.start();
+        timer2 = new Timer(100,null);
+        timer2.addActionListener(this);
         
         fondo = new PanelFondo();
         this.getContentPane().add(fondo);
@@ -18,5 +27,10 @@ public class Ventana extends JFrame{
         this.setLocationRelativeTo(null);
         this.setResizable(false);
         this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("*");
     }
 }
