f44004f3c73e5c8e551aaa396c4435d486bafb25
JackFeels <107821314+JackFeels@users.noreply.github.com>
2022-12-11 19:35:54 -0300

    Termino de javadoc
    termine lo que faltaba del javadoc

--- Proyecto/src/Utils/Vector2.java
+++ Proyecto/src/Utils/Vector2.java
@@ -6,25 +6,43 @@ package Utils;
  */
 public class Vector2{
     public float x,y;
-    
+    /**
+     * Constructor de clase Vector2
+     * @param x: coordenada en el eje x
+     * @param y: coordenada en el eje y
+     */
     public Vector2(float x, float y){
         this.x = x;
         this.y = y;
     }
-    
+    /**
+     * Metodo dot que calcula el producto punto entre dos vectores
+     * @param a vector a
+     * @param b vector b
+     * @return producto punto
+     */
     public static float dot(Vector2 a, Vector2 b){
         return a.x * b.x + a.y * b.y;
     }
-    
+    /**
+     * Metodo escalar que multiplica por un escalar al vector
+     * @param f float escalar
+     */
     public void escalar(float f){
         x *= f;
         y *= f;
     }
-    
+    /**
+     * Metodo magnitud que calcula la magnitud del vector
+     * @return magnitud del vector
+     */
     public float magnitud(){
         return (float)Math.sqrt(x*x + y*y);
     }
-    
+    /**
+     * Metodo normalizar que normaliza un vector
+     * @see magnitud
+     */
     public void normalizar(){
         float mag = magnitud();
         x /= mag;
